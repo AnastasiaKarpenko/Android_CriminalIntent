f2341421d4b409afa5c35dc15270f203f0aa1d61
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-02-14 14:07:54 +0200

    cleaning the imports

--- app/src/main/java/ws/tilda/anastasia/criminalintent/CrimeListFragment.java
+++ app/src/main/java/ws/tilda/anastasia/criminalintent/CrimeListFragment.java
@@ -5,8 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.ViewHolder;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,8 +12,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
-
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 
 public class CrimeListFragment extends Fragment {
