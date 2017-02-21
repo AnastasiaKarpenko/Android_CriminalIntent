46af641be89213d1ce31228ca89c3c6255fa86a7
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-02-21 10:26:30 +0200

    edited mistake in the loop

--- app/src/main/java/ws/tilda/anastasia/criminalintent/CrimeFragment.java
+++ app/src/main/java/ws/tilda/anastasia/criminalintent/CrimeFragment.java
@@ -48,6 +48,14 @@ public class CrimeFragment extends Fragment {
     }
 
     @Override
+    public void onPause() {
+        super.onPause();
+
+        CrimeLab.get(getActivity())
+                .updateCrime(mCrime);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
@@ -90,7 +98,8 @@ public class CrimeFragment extends Fragment {
             public void onCheckedChanged(CompoundButton buttonView,
                                          boolean isChecked) {
                 mCrime.setSolved(isChecked);
-            } });
+            }
+        });
 
         return v;
     }
